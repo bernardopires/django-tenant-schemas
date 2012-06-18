4568969e88dd27b813037cf51ce47b1517a68393
Bernardo Pires <carneiro.be@gmail.com>
2012-06-18 12:42:34 +0200

    BaseSchemataCommand now allows running a command on a particular schema

--- django_schemata/management/commands/sync_schemata.py
+++ django_schemata/management/commands/sync_schemata.py
@@ -1,3 +1,4 @@
+from optparse import make_option
 from django.conf import settings
 from django.core.management import call_command, get_commands, load_command_class
 from django.core.management.base import BaseCommand
@@ -8,7 +9,7 @@ class BaseSchemataCommand(BaseCommand):
     Generic command class useful for iterating any existing command
     over all schemata. The actual command name is expected in the
     class variable COMMAND_NAME of the subclass.
-    """ 
+    """
     def __new__(cls, *args, **kwargs):
         """
         Sets option_list and help dynamically.
@@ -19,6 +20,9 @@ class BaseSchemataCommand(BaseCommand):
         cmdclass = load_command_class(get_commands()[obj.COMMAND_NAME], obj.COMMAND_NAME) 
         # inherit the options from the original command
         obj.option_list = cmdclass.option_list
+        obj.option_list = BaseCommand.option_list + (
+            make_option("-s", "--schema", dest="schema_name"),
+        )
         # prepend the command's original help with the info about schemata iteration
         obj.help = "Calls %s for all registered schemata. You can use regular %s options. " \
                    "Original help for %s: %s" \
@@ -26,22 +30,34 @@ class BaseSchemataCommand(BaseCommand):
                       getattr(cmdclass, 'help', 'none'))
         return obj
 
+    def execute_command(self, domain_name, command_name, *args, **options):
+        print
+        print self.style.NOTICE("=== Switching to domain ")\
+              + self.style.SQL_TABLE(domain_name)\
+        + self.style.NOTICE(" then calling %s:" % command_name)
+
+        # sets the schema for the connection
+        connection.set_schemata_domain(domain_name)
+
+        # call the original command with the args it knows
+        call_command(command_name, *args, **options)
+
+    def get_schema_name_from_domain(self, schema_name):
+        for domain_name in settings.SCHEMATA_DOMAINS:
+            if settings.SCHEMATA_DOMAINS[domain_name]["schema_name"] == schema_name:
+                return domain_name
+
     def handle(self, *args, **options):
         """
         Iterates a command over all registered schemata.
         """
-        for domain_name in settings.SCHEMATA_DOMAINS:
-
-            print
-            print self.style.NOTICE("=== Switching to domain ") \
-                + self.style.SQL_TABLE(domain_name) \
-                + self.style.NOTICE(" then calling %s:" % self.COMMAND_NAME)
-
-            # sets the schema for the connection
-            connection.set_schemata_domain(domain_name)
-
-            # call the original command with the args it knows
-            call_command(self.COMMAND_NAME, *args, **options)
+        # only run on a particular schema?
+        if options['schema_name']:
+            self.execute_command(self.get_schema_name_from_domain(options['schema_name']), self.COMMAND_NAME, *args, **options)
+        else:
+            for domain_name in settings.SCHEMATA_DOMAINS:
+                options['schema_name'] = settings.SCHEMATA_DOMAINS[domain_name]["schema_name"]
+                self.execute_command(domain_name, self.COMMAND_NAME, *args, **options)
 
 
 class Command(BaseSchemataCommand):
--- django_schemata/middleware.py
+++ django_schemata/middleware.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import connection
 
 class SchemataMiddleware(object):
@@ -10,6 +11,17 @@ class SchemataMiddleware(object):
         hostname_without_port = request.get_host().split(':')[0]
         request.schema_domain_name = hostname_without_port
         request.schema_domain = connection.set_schemata_domain(request.schema_domain_name)
+        #print request.schema_domain["schema_name"]
+
+        print settings.ROOT_URLCONF
+
+
+        if request.schema_domain["schema_name"] != "public" and request.path_info[-1] == '/':
+            request.path_info = "/firm" + request.path_info[1:]
+        #   settings.ROOT_URLCONF = settings.ROOT_SCHEMA_URLCONF
+        #else:
+        #    settings.ROOT_URLCONF = settings.ROOT_NO_SCHEMA_URLCONF
+
 
     # The question remains whether it's necessary to unset the schema
     # when the request finishes...

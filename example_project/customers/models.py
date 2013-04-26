943becdc82e13672d6c31023c3e489fe556611b5
Caio Ariede <caio.ariede@gmail.com>
2013-04-26 13:45:35 -0300

    Added an example project to help testing

---
+++ example_project/customers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from tenant_schemas.models import TenantMixin
+
+
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+    paid_until =  models.DateField()
+    on_trial = models.BooleanField()
+    created_on = models.DateField(auto_now_add=True)
+
+    # default true, schema will be automatically created and synced when it is saved
+    auto_create_schema = True

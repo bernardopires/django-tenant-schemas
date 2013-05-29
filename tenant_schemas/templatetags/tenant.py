d797fa956e95927633d376f8a0eb0c956bc0a19e
Stefan Baxter <stefan@activitystream.com>
2013-05-29 15:35:08 +0000

    www fixes

--- tenant_schemas/templatetags/tenant.py
+++ tenant_schemas/templatetags/tenant.py
@@ -10,15 +10,14 @@ class SchemaURLNode(django.template.defaulttags.URLNode):
     def __init__(self, url_node):
         super(SchemaURLNode, self).__init__(url_node.view_name, url_node.args, url_node.kwargs, url_node.asvar)
 
-
-def render(self, context):
-    url = super(SchemaURLNode, self).render(context)
-    return tenant_schemas.utils.clean_tenant_url(url)
+    def render(self, context):
+        url = super(SchemaURLNode, self).render(context)
+        return tenant_schemas.utils.clean_tenant_url(url)
 
 
 @register.tag
 def url(parser, token):
-    return SchemaURLNode(django.template.defaulttags.url(parser,token))
+    return SchemaURLNode(django.template.defaulttags.url(parser, token))
 
 
 @register.assignment_tag(takes_context=True)

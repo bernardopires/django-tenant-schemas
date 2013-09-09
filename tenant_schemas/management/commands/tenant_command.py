6caf655fe170ed3695d9e726bcc582b57210e229
Stephen J. Fuhry <steve@montmere.com>
2013-09-09 15:09:13 -0400

    option to specify --tenant arg

--- tenant_schemas/management/commands/tenant_command.py
+++ tenant_schemas/management/commands/tenant_command.py
@@ -1,4 +1,5 @@
 from getpass import getpass
+from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
@@ -7,6 +8,16 @@ from django.db import connection
 from django.utils.six.moves import input
 
 class Command(BaseCommand):
+   
+    help = "Wrapper around django commands for use with an individual tenant" 
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            "--tenant", 
+            dest = "tenant_schema",
+            help = "specify tenant schema", 
+        ),
+    )
 
     def handle(self, command=None, target=None, *args, **options):
         from tenant_schemas.utils import get_tenant_model
@@ -23,12 +34,15 @@ https://django-tenant-schemas.readthedocs.org/en/latest/use.html#creating-a-tena
         
         #tenstrings = ["%s - %s" % (t.pk, t.domain_url,) for t in alltenants]
         
-        while True:
-            tenant_schema = input("Enter Tenant Schema ('?' to list schemas): ")
-            if tenant_schema == '?':
-                print '\n'.join(["%s - %s" % (t.schema_name, t.domain_url,) for t in alltenants])
-            else:
-                break
+        if options['tenant_schema']:
+            tenant_schema = options['tenant_schema']
+        else:
+            while True:
+                tenant_schema = input("Enter Tenant Schema ('?' to list schemas): ")
+                if tenant_schema == '?':
+                    print '\n'.join(["%s - %s" % (t.schema_name, t.domain_url,) for t in alltenants])
+                else:
+                    break
 
         if tenant_schema not in [t.schema_name for t in alltenants]:
             raise CommandError("Invalid tenant schema, '%s'" % (tenant_schema,))

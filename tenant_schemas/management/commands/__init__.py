4be1f0889f8a3f92ca30306438034c02a9eb1a90
Bernardo Pires <carneiro.be@gmail.com>
2013-10-04 12:49:29 +0200

    pep8

--- tenant_schemas/management/commands/__init__.py
+++ tenant_schemas/management/commands/__init__.py
@@ -1,10 +1,10 @@
 from optparse import make_option
-from django.conf import settings
 from django.core.management import call_command, get_commands, load_command_class
-from django.core.management.base import BaseCommand, NoArgsCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db import connection
-from django.utils.six.moves import input
 from tenant_schemas.utils import get_tenant_model, get_public_schema_name
+from django.core.management.base import CommandError
+from django.utils.six.moves import input
 
 
 class BaseTenantCommand(BaseCommand):
@@ -78,9 +78,9 @@ class InteractiveTenantOption(object):
 
     def get_tenant_from_options_or_interactive(self, **options):
         TenantModel = get_tenant_model()
-        allTenants = TenantModel.objects.all()
+        all_tenants = TenantModel.objects.all()
 
-        if not allTenants:
+        if not all_tenants:
             raise CommandError("""There are no tenants in the system.
 To learn how create a tenant, see:
 https://django-tenant-schemas.readthedocs.org/en/latest/use.html#creating-a-tenant""")
@@ -91,11 +91,11 @@ https://django-tenant-schemas.readthedocs.org/en/latest/use.html#creating-a-tena
             while True:
                 tenant_schema = input("Enter Tenant Schema ('?' to list schemas): ")
                 if tenant_schema == '?':
-                    print '\n'.join(["%s - %s" % (t.schema_name, t.domain_url,) for t in allTenants])
+                    print '\n'.join(["%s - %s" % (t.schema_name, t.domain_url,) for t in all_tenants])
                 else:
                     break
 
-        if tenant_schema not in [t.schema_name for t in allTenants]:
+        if tenant_schema not in [t.schema_name for t in all_tenants]:
             raise CommandError("Invalid tenant schema, '%s'" % (tenant_schema,))
 
         return TenantModel.objects.get(schema_name=tenant_schema)
@@ -118,40 +118,3 @@ class TenantWrappedCommand(InteractiveTenantOption, BaseCommand):
         connection.set_tenant(tenant)
 
         self.command_instance.execute(*args, **options)
-
-
-class SyncCommon(NoArgsCommand):
-    option_list = (
-        make_option('--tenant', action='store_true', dest='tenant', default=False,
-                    help='Tells Django to populate only tenant applications.'),
-        make_option('--shared', action='store_true', dest='shared', default=False,
-                    help='Tells Django to populate only shared applications.'),
-        make_option("-s", "--schema", dest="schema_name"),
-    )
-
-    def handle_noargs(self, **options):
-        self.sync_tenant = options.get('tenant')
-        self.sync_public = options.get('shared')
-        self.schema_name = options.get('schema_name')
-        self.installed_apps = settings.INSTALLED_APPS
-        self.options = options
-
-        if self.schema_name:
-            if self.sync_public:
-                raise CommandError("schema should only be used with the --tenant switch.")
-            elif self.schema_name == get_public_schema_name():
-                self.sync_public = True
-            else:
-                self.sync_tenant = True
-        elif not self.sync_public and not self.sync_tenant:
-            # no options set, sync both
-            self.sync_tenant = True
-            self.sync_public = True
-
-        if hasattr(settings, 'TENANT_APPS'):
-            self.tenant_apps = settings.TENANT_APPS
-        if hasattr(settings, 'SHARED_APPS'):
-            self.shared_apps = settings.SHARED_APPS
-
-    def _notice(self, output):
-        self.stdout.write(self.style.NOTICE(output))

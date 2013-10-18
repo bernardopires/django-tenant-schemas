04f0de916550e62ec1da60ba23a045d25e6b82b1
Bernardo Pires <carneiro.be@gmail.com>
2013-10-18 10:49:53 +0200

    fixed type, fixes #83

--- tenant_schemas/management/commands/migrate.py
+++ tenant_schemas/management/commands/migrate.py
@@ -3,5 +3,5 @@ from django.core.management.base import NoArgsCommand, CommandError
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        raise CommandError("migrate has been disabled, use migrates_schemas instead. Please read the "
+        raise CommandError("migrate has been disabled, use migrate_schemas instead. Please read the "
                            "documentation if you don't know why you shouldn't call migrate directly!")

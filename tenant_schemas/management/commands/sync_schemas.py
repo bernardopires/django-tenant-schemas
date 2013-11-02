4c3f14b7aea53310fcd2c10cade70d93e6bf69af
Bernardo Pires <carneiro.be@gmail.com>
2013-11-02 18:10:32 +0100

    sync_schemas simplified, no longer needs to manually tell which apps need to be synced thanks to router

--- tenant_schemas/management/commands/sync_schemas.py
+++ tenant_schemas/management/commands/sync_schemas.py
@@ -20,10 +20,7 @@ class Command(SyncCommon):
         if "south" in settings.INSTALLED_APPS:
             self.options["migrate"] = False
 
-        # save original settings
-        for model in get_models(include_auto_created=True):
-            setattr(model._meta, 'was_managed', model._meta.managed)
-
+        # Content types may be different on tenants, so reset the cache
         ContentType.objects.clear_cache()
 
         if self.sync_public:
@@ -31,32 +28,12 @@ class Command(SyncCommon):
         if self.sync_tenant:
             self.sync_tenant_apps(self.schema_name)
 
-        # restore settings
-        for model in get_models(include_auto_created=True):
-            model._meta.managed = model._meta.was_managed
-
-    def _set_managed_apps(self, included_apps):
-        """ sets which apps are managed by syncdb """
-        for model in get_models(include_auto_created=True):
-            model._meta.managed = False
-
-        verbosity = int(self.options.get('verbosity'))
-        for app_model in get_apps():
-            app_name = app_model.__name__.replace('.models', '')
-            if hasattr(app_model, 'models') and app_name in included_apps:
-                for model in get_models(app_model, include_auto_created=True):
-                    model._meta.managed = model._meta.was_managed
-                    if model._meta.managed and verbosity >= 3:
-                        self._notice("=== Include Model: %s: %s" % (app_name, model.__name__))
-
     def _sync_tenant(self, tenant):
         self._notice("=== Running syncdb for schema: %s" % tenant.schema_name)
         connection.set_tenant(tenant, include_public=False)
         SyncdbCommand().execute(**self.options)
 
     def sync_tenant_apps(self, schema_name=None):
-        apps = self.tenant_apps or self.installed_apps
-        self._set_managed_apps(apps)
         if schema_name:
             tenant = get_tenant_model().objects.filter(schema_name=schema_name).get()
             self._sync_tenant(tenant)
@@ -69,7 +46,5 @@ class Command(SyncCommon):
                 self._sync_tenant(tenant)
 
     def sync_public_apps(self):
-        apps = self.shared_apps or self.installed_apps
-        self._set_managed_apps(apps)
         SyncdbCommand().execute(**self.options)
         self._notice("=== Running syncdb for schema public")

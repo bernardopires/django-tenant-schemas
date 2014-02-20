76095afc45c9eb661cfb1df47dcda6c18c6d26a0
Patrick Shields <shields.j.patrick@gmail.com>
2014-02-20 13:37:34 -0500

    Added a put method.

--- tenant_schemas/test/client.py
+++ tenant_schemas/test/client.py
@@ -24,9 +24,19 @@ class TenantRequestFactory(RequestFactory):
     def patch(self, path, data={}, **extra):
         if 'HTTP_HOST' not in extra:
             extra['HTTP_HOST'] = self.tenant.domain_url
-        extra["content_type"] = "application/json"
+        if 'content_type' not in extra:
+            extra["content_type"] = "application/json"
 
         return super(TenantRequestFactory, self).patch(path, data, **extra)
+
+    def put(self, path, data={}, **extra):
+        if 'HTTP_HOST' not in extra:
+            extra['HTTP_HOST'] = self.tenant.domain_url
+        if 'content_type' not in extra:
+            extra["content_type"] = "application/json"
+
+        return super(TenantRequestFactory, self).patch(path, data, **extra)
+
 
 class TenantClient(Client):
     tm = TenantMiddleware()
@@ -50,6 +60,15 @@ class TenantClient(Client):
     def patch(self, path, data={}, **extra):
         if 'HTTP_HOST' not in extra:
             extra['HTTP_HOST'] = self.tenant.domain_url
-        extra["content_type"] = "application/json"
+        if 'content_type' not in extra:
+            extra["content_type"] = "application/json"
+
+        return super(TenantClient, self).patch(path, data, **extra)
+
+    def put(self, path, data={}, **extra):
+        if 'HTTP_HOST' not in extra:
+            extra['HTTP_HOST'] = self.tenant.domain_url
+        if 'content_type' not in extra:
+            extra["content_type"] = "application/json"
 
         return super(TenantClient, self).patch(path, data, **extra)

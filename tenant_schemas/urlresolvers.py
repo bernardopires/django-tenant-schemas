c4bbd599107e2d0df8944eb735e156e565720d69
Bernardo Pires <carneiro.be@gmail.com>
2014-04-23 23:04:02 +0200

    removing deprecation warning

--- tenant_schemas/urlresolvers.py
+++ tenant_schemas/urlresolvers.py
@@ -7,11 +7,6 @@ from tenant_schemas.utils import clean_tenant_url
 
 def reverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None,
             current_app=None):
-    if hasattr(settings, 'PUBLIC_SCHEMA_URL_TOKEN'):
-        warnings.warn("PUBLIC_SCHEMA_URL_TOKEN is deprecated. "
-                      "Use PUBLIC_SCHEMA_URLCONF instead.",
-                      category=DeprecationWarning)
-
     url = reverse_default(viewname, urlconf, args, kwargs, prefix, current_app)
     return clean_tenant_url(url)
 

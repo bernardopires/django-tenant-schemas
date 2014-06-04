6a2aeb5c63aa29d646cfa9f44887f426b8cf6ba4
Christian Felipe <cfelipe@qdqmedia.com>
2014-06-04 14:19:35 +0200

    allow migrate for dbs not managed by tenant

--- tenant_schemas/management/commands/syncdb.py
+++ tenant_schemas/management/commands/syncdb.py
@@ -12,8 +12,8 @@ class Command(syncdb.Command):
     def handle(self, *args, **options):
         database = options.get('database', 'default')
         if settings.DATABASES[database]['ENGINE'] == 'tenant_schemas.postgresql_backend':
-            raise CommandError("syncdb has been disabled, for the database {}. "
+            raise CommandError("syncdb has been disabled, for database '{}'. "
                                "Use sync_schemas instead. Please read the "
                                "documentation if you don't know why "
-                               "you shouldn't call syncdb directly!")
+                               "you shouldn't call syncdb directly!".format(database))
         super(Command, self).handle(*args, **options)

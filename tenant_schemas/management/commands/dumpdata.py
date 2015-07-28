0aba23892b84ea6d61df38a3dcc3771f4a8f0938
Gary Reynolds <gary@touch.asn.au>
2015-07-29 09:20:32 +1000

    Overload the dumpdata and loaddata management commands to provide the -s schema switch.
    If you try to pass other options (such as -n for natural keys) when wrapping dumpdata with tenant_command they are lost and cause failures.

---
+++ tenant_schemas/management/commands/dumpdata.py
@@ -0,0 +1,11 @@
+from django.core.management.commands.dumpdata import Command as DataCommand
+from django.db import connection
+
+from tenant_schemas.management.commands import InteractiveTenantOption
+
+
+class Command(InteractiveTenantOption, DataCommand):
+    def handle(self, *args, **options):
+        tenant = self.get_tenant_from_options_or_interactive(**options)
+        connection.set_tenant(tenant)
+        return super(Command, self).handle(*args, **options)

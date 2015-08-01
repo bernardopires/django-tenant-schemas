6eb1ebcdf40b009a8d8b3876d3b4621e6de43e5a
Gary Reynolds <gary@touch.asn.au>
2015-08-01 23:03:43 +1000

    Remove conflicting option "database" from SyncCommon. Fixes #272.

--- tenant_schemas/management/commands/__init__.py
+++ tenant_schemas/management/commands/__init__.py
@@ -138,8 +138,6 @@ class SyncCommon(BaseCommand):
         make_option('--migration_name', action='store', dest='migration_name', nargs='?',
                     help=('Database state will be brought to the state after that '
                           'migration. Use the name "zero" to unapply all migrations.')),
-        make_option('--database', action='store', dest='database', default=DEFAULT_DB_ALIAS,
-                    help='Nominates a database to synchronize. Defaults to the "default" database.'),
         make_option("-s", "--schema", dest="schema_name"),
     )
 

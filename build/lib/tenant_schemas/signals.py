997636435fea5e6ff88ef098ff6d462161de521f
David Miranda <davidmir@sentilant.com>
2015-10-29 09:58:44 +0000

    Clean

--- build/lib/tenant_schemas/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch import Signal
-
-post_schema_sync = Signal(providing_args=['tenant'])
-post_schema_sync.__doc__ = """
-Sent after a tenant has been saved, its schema created and synced
-"""

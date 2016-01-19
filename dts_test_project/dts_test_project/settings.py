1743b4867b883bfd499ae5a0024940ca7ec43b47
Gary Reynolds <gary@touch.asn.au>
2016-01-19 17:03:18 +1100

    Add LOGGING configuration to test project

--- dts_test_project/dts_test_project/settings.py
+++ dts_test_project/dts_test_project/settings.py
@@ -122,3 +122,34 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)-7s %(asctime)s %(message)s',
+        },
+    },
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['null'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}

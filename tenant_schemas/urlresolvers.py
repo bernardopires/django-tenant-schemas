c8921aa571074819201bb86c78b6d3617d72b7fd
Jacob Haslehurst <jacob@haslehurst.net>
2016-03-18 15:06:56 +1100

    fix `reverse` for Django 1.9
    `reverse` no longer accepts the `prefix` argument
    https://docs.djangoproject.com/en/1.9/ref/urlresolvers/#reverse

--- tenant_schemas/urlresolvers.py
+++ tenant_schemas/urlresolvers.py
@@ -5,9 +5,8 @@ from django.utils.functional import lazy
 from tenant_schemas.utils import clean_tenant_url
 
 
-def reverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None,
-            current_app=None):
-    url = reverse_default(viewname, urlconf, args, kwargs, prefix, current_app)
+def reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None):
+    url = reverse_default(viewname, urlconf, args, kwargs, current_app)
     return clean_tenant_url(url)
 
 reverse_lazy = lazy(reverse, str)

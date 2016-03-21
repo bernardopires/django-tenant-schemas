2e58510b558eead203572da20e0b73f865aae3fd
Jacob Haslehurst <jacob@haslehurst.net>
2016-03-21 11:42:57 +1100

    explicitly pass kwargs to django reverse

--- tenant_schemas/urlresolvers.py
+++ tenant_schemas/urlresolvers.py
@@ -6,7 +6,13 @@ from tenant_schemas.utils import clean_tenant_url
 
 
 def reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None):
-    url = reverse_default(viewname, urlconf, args, kwargs, current_app)
+    url = reverse_default(
+        viewname=viewname, 
+        urlconf=urlconf, 
+        args=args, 
+        kwargs=kwargs, 
+        current_app=current_app
+    )
     return clean_tenant_url(url)
 
 reverse_lazy = lazy(reverse, str)

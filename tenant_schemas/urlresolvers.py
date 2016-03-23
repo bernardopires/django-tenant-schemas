0be38470e5b21d165d3110b27517cbd0a21bf9d8
Jacob Haslehurst <jacob@haslehurst.net>
2016-03-23 12:13:05 +1100

    re-add `prefix` argument to `reverse`

--- tenant_schemas/urlresolvers.py
+++ tenant_schemas/urlresolvers.py
@@ -5,7 +5,8 @@ from django.utils.functional import lazy
 from tenant_schemas.utils import clean_tenant_url
 
 
-def reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None):
+def reverse(viewname, urlconf=None, args=None, kwargs=None, prefix=None,
+            current_app=None):
     url = reverse_default(
         viewname=viewname, 
         urlconf=urlconf, 

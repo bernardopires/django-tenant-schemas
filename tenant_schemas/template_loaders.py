bernardopires/django-tenant-schemas

commit cb8862d026750a5543867e246c0e8de09b21c4fa

Remove Django < 1.8 compatibility
 - resolves template loading in Django 1.8+ (thanks to @tomturner)
 - removed code branches based on django.VERSION value
 - updated documentation to remove details of removed functionality

Closes #289

tenant_schemas/template_loaders.py
@@ -6,81 +6,92 @@ multi-tenant setting
 import hashlib
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.template.base import TemplateDoesNotExist
-from django.template.loader import (BaseLoader, get_template_from_string,
-                                    find_template_loader, make_origin)
+from django.template.base import TemplateDoesNotExist, Template
 from django.utils.encoding import force_bytes
 from django.utils._os import safe_join
 from django.db import connection
-from tenant_schemas.postgresql_backend.base import FakeTenant
+from django.template.loaders.base import Loader as BaseLoader
 
+from tenant_schemas.postgresql_backend.base import FakeTenant
+import django
 
 class CachedLoader(BaseLoader):
     is_usable = True
 
-    def __init__(self, loaders):
+    def __init__(self, engine, loaders):
         self.template_cache = {}
-        self._loaders = loaders
-        self._cached_loaders = []
+        self.find_template_cache = {}
+        self.loaders = engine.get_template_loaders(loaders)
+        super(CachedLoader, self).__init__(engine)
 
-    @property
-    def loaders(self):
-        # Resolve loaders on demand to avoid circular imports
-        if not self._cached_loaders:
-            # Set self._cached_loaders atomically. Otherwise, another thread
-            # could see an incomplete list. See #17303.
-            cached_loaders = []
-            for loader in self._loaders:
-                cached_loaders.append(find_template_loader(loader))
-            self._cached_loaders = cached_loaders
-        return self._cached_loaders
+    @staticmethod
+    def cache_key(template_name, template_dirs):
+        if connection.tenant and template_dirs:
+            return '-'.join([str(connection.tenant.pk), template_name,
+                             hashlib.sha1(force_bytes('|'.join(template_dirs))).hexdigest()])
+        if template_dirs:
+            # If template directories were specified, use a hash to differentiate
+            return '-'.join([template_name, hashlib.sha1(force_bytes('|'.join(template_dirs))).hexdigest()])
+        else:
+            return template_name
 
     def find_template(self, name, dirs=None):
-        for loader in self.loaders:
-            try:
-                template, display_name = loader(name, dirs)
-                return template, make_origin(display_name, loader, name, dirs)
-            except TemplateDoesNotExist:
-                pass
-        raise TemplateDoesNotExist(name)
+        """
+        Helper method. Lookup the template :param name: in all the configured loaders
+        """
+        key = self.cache_key(name, dirs)
+        try:
+            result = self.find_template_cache[key]
+        except KeyError:
+            result = None
+            for loader in self.loaders:
+                try:
+                    template, display_name = loader(name, dirs)
+                except TemplateDoesNotExist:
+                    pass
+                else:
+                    origin = self.engine.make_origin(display_name, loader, name, dirs)
+                    result = template, origin
+                    break
+        self.find_template_cache[key] = result
+        if result:
+            return result
+        else:
+            self.template_cache[key] = TemplateDoesNotExist
+            raise TemplateDoesNotExist(name)
 
     def load_template(self, template_name, template_dirs=None):
-        if connection.tenant:
-            key = '-'.join([str(connection.tenant.pk), template_name])
-        else:
-            key = template_name
-        if template_dirs:
-            # If template directories were specified, use a hash to
-            # differentiate
-            if connection.tenant:
-                key = '-'.join([str(connection.tenant.pk), template_name,
-                                hashlib.sha1(force_bytes('|'.join(template_dirs))).hexdigest()])
-            else:
-                key = '-'.join([template_name, hashlib.sha1(force_bytes('|'.join(template_dirs))).hexdigest()])
-
-        if key not in self.template_cache:
+        key = self.cache_key(template_name, template_dirs)
+        template_tuple = self.template_cache.get(key)
+        # A cached previous failure:
+        if template_tuple is TemplateDoesNotExist:
+            raise TemplateDoesNotExist
+        elif template_tuple is None:
             template, origin = self.find_template(template_name, template_dirs)
             if not hasattr(template, 'render'):
                 try:
-                    template = get_template_from_string(template, origin, template_name)
+                    template = Template(template, origin, template_name, self.engine)
                 except TemplateDoesNotExist:
                     # If compiling the template we found raises TemplateDoesNotExist,
                     # back off to returning the source and display name for the template
                     # we were asked to load. This allows for correct identification (later)
                     # of the actual template that does not exist.
-                    return template, origin
-            self.template_cache[key] = template
-        return self.template_cache[key], None
+                    self.template_cache[key] = (template, origin)
+            self.template_cache[key] = (template, None)
+        return self.template_cache[key]
 
     def reset(self):
-        "Empty the template cache."
+        """
+        Empty the template cache.
+        """
         self.template_cache.clear()
 
 
 class FilesystemLoader(BaseLoader):
     is_usable = True
 
-    def get_template_sources(self, template_name, template_dirs=None):
+    @staticmethod
+    def get_template_sources(template_name, template_dirs=None):
         """
         Returns the absolute paths to "template_name", when appended to each
         directory in "template_dirs". Any paths that don't lie inside one of the
@@ -97,9 +108,9 @@ class FilesystemLoader(BaseLoader):
         for template_dir in template_dirs:
             try:
                 if '%s' in template_dir:
-                    yield safe_join(template_dir % connection.tenant.domain_url, template_name)
+                    yield safe_join(template_dir % connection.tenant.schema_name, template_name)
                 else:
-                    yield safe_join(template_dir, connection.tenant.domain_url, template_name)
+                    yield safe_join(template_dir, connection.tenant.schema_name, template_name)
             except UnicodeDecodeError:
                 # The template dir name was a bytestring that wasn't valid UTF-8.
                 raise
@@ -114,7 +125,7 @@ class FilesystemLoader(BaseLoader):
         for filepath in self.get_template_sources(template_name, template_dirs):
             try:
                 with open(filepath, 'rb') as fp:
-                    return (fp.read().decode(settings.FILE_CHARSET), filepath)
+                    return fp.read().decode(settings.FILE_CHARSET), filepath
             except IOError:
                 tried.append(filepath)
         if tried:

dts_test_project/dts_test_project/settings.py
@@ -2,10 +2,10 @@
 Django settings for dts_test_project project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/1.8/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'cl1)b#c&xmm36z3e(quna-vb@ab#&gpjtdjtpyzh!qn%bc^xxn'
@@ -48,11 +48,7 @@ TENANT_MODEL = "customers.Client"  # app.Model
 
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
-import django
-if django.VERSION >= (1, 7, 0):
-    INSTALLED_APPS = list(set(TENANT_APPS + SHARED_APPS))
-else:
-    INSTALLED_APPS = TENANT_APPS + SHARED_APPS + ('tenant_schemas',)
+INSTALLED_APPS = list(set(TENANT_APPS + SHARED_APPS))
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -69,7 +65,7 @@ WSGI_APPLICATION = 'dts_test_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -104,7 +100,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -118,7 +114,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
 

examples/tenant_tutorial/templates/index_public.html
@@ -18,11 +18,7 @@
     </ul><br>
     <p>Just run the command below on your shell to sync <code>SHARED_APPS</code>. Make sure your environment
     has <code>Django</code> and <code>django-tenant-schemas</code> available.</p>
-{% if DJANGO17 %}
     <pre>$ python manage.py migrate_schemas --shared</pre>
-{% else %}
-    <pre>$ python manage.py sync_schemas --shared</pre>
-{% endif %}
     <p>When you're done refresh this page.</p>
 {% elif no_public_tenant %}
     <h2>Second Step: Create a public tenant</h2>

examples/tenant_tutorial/tenant_tutorial/settings.py
@@ -143,11 +143,7 @@ TENANT_APPS = (
 
 TENANT_MODEL = "customers.Client"  # app.Model
 
-import django
-if django.VERSION >= (1, 7, 0):
-    INSTALLED_APPS = list(set(TENANT_APPS + SHARED_APPS))
-else:
-    INSTALLED_APPS = TENANT_APPS + SHARED_APPS + ('tenant_schemas',)
+INSTALLED_APPS = list(set(TENANT_APPS + SHARED_APPS))
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 

examples/tenant_tutorial/tenant_tutorial/urls_public.py
@@ -1,4 +1,3 @@
-import django
 from django.conf.urls import url
 from tenant_tutorial.views import HomeView
 
@@ -6,6 +5,3 @@ from tenant_tutorial.views import HomeView
 urlpatterns = [
     url(r'^$', HomeView.as_view()),
 ]
-
-if django.VERSION < (1, 9, 0):
-    urlpatterns = django.conf.urls.patterns('', *urlpatterns)

examples/tenant_tutorial/tenant_tutorial/urls_tenants.py
@@ -1,11 +1,6 @@
-import django
-from django.conf.urls import url
 from customers.views import TenantView
-
+from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', TenantView.as_view()),
 ]
-
-if django.VERSION < (1, 9, 0):
-    urlpatterns = django.conf.urls.patterns('', *urlpatterns)

examples/tenant_tutorial/tenant_tutorial/views.py
@@ -1,9 +1,9 @@
-import django
+from customers.models import Client
 from django.conf import settings
 from django.db import utils
 from django.views.generic import TemplateView
+
 from tenant_schemas.utils import remove_www
-from customers.models import Client
 
 
 class HomeView(TemplateView):
@@ -13,9 +13,6 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
 
         hostname_without_port = remove_www(self.request.get_host().split(':')[0])
-
-        if django.VERSION >= (1, 7, 0):
-            context['DJANGO17'] = True
 
         try:
             Client.objects.get(schema_name='public')

setup.py
@@ -8,8 +8,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-__version__ = "1.4.0"
-
 setup(
     name='django-tenant-schemas',
     version=get_git_version(),
@@ -20,7 +18,6 @@ setup(
         'tenant_schemas.postgresql_backend',
         'tenant_schemas.management',
         'tenant_schemas.management.commands',
-        'tenant_schemas.management.commands.legacy',
         'tenant_schemas.templatetags',
         'tenant_schemas.test',
         'tenant_schemas.tests',
@@ -36,7 +33,7 @@ setup(
         'Programming Language :: Python',
     ],
     install_requires=[
-        'Django >= 1.6.0',
+        'Django >= 1.8.0',
         'psycopg2',
     ],
     zip_safe=False,

commit 1a48f6c57b6b1cde3c153a5f9bc3ba7235ad146c

Fix regression introduced in cb8862d
 - path construction changed from domain_url to schema_name

tenant_schemas/template_loaders.py
@@ -108,9 +108,9 @@ class FilesystemLoader(BaseLoader):
         for template_dir in template_dirs:
             try:
                 if '%s' in template_dir:
-                    yield safe_join(template_dir % connection.tenant.schema_name, template_name)
+                    yield safe_join(template_dir % connection.tenant.domain_url, template_name)
                 else:
-                    yield safe_join(template_dir, connection.tenant.schema_name, template_name)
+                    yield safe_join(template_dir, connection.tenant.domain_url, template_name)
             except UnicodeDecodeError:
                 # The template dir name was a bytestring that wasn't valid UTF-8.
                 raise
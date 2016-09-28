490da5a6da039c952d69820fb59a7ae383e40004
Mateu Cànaves <mateu.canaves@gmail.com>
2016-09-28 09:39:46 +0200

    pep8

--- tenant_schemas/management/commands/migrate_schemas.py
+++ tenant_schemas/management/commands/migrate_schemas.py
@@ -2,17 +2,15 @@ import django
 from django.core.management.commands.migrate import Command as MigrateCommand
 from django.db import connection
 
+from tenant_schemas.management.commands import SyncCommon
 from tenant_schemas.migration_executors import get_executor
+from tenant_schemas.utils import get_public_schema_name, get_tenant_model, schema_exists
 
 if django.VERSION >= (1, 9, 0):
     from django.db.migrations.exceptions import MigrationSchemaMissing
 else:
     class MigrationSchemaMissing(django.db.utils.DatabaseError):
         pass
-
-
-from tenant_schemas.management.commands import SyncCommon
-from tenant_schemas.utils import get_tenant_model, get_public_schema_name, schema_exists
 
 
 class Command(SyncCommon):

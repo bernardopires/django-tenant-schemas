ea21f077ae9d81e6fb3b6466eba028d19a5e394d
Gary Reynolds <gary@touch.asn.au>
2017-02-24 22:50:31 +1100

    Update existing test cases due to extra Errors

--- tenant_schemas/tests/test_apps.py
+++ tenant_schemas/tests/test_apps.py
@@ -86,6 +86,9 @@ class AppConfigTests(TestCase):
             Error("TENANT_APPS is empty.",
                   hint="Maybe you don't need this app?",
                   id="tenant_schemas.E001"),
+            Error("You have INSTALLED_APPS that are not in either of "
+                  "TENANT_APPS or SHARED_APPS",
+                  hint=['dts_test_app']),
         ])
 
     @override_settings(PG_EXTRA_SEARCH_PATHS=['public', 'demo1', 'demo2'])
@@ -100,11 +103,20 @@ class AppConfigTests(TestCase):
             Critical("Do not include tenant schemas (demo1, demo2) on PG_EXTRA_SEARCH_PATHS."),
         ])
 
-    @override_settings(SHARED_APPS=())
+    @override_settings(SHARED_APPS=(), INSTALLED_APPS=(
+        'tenant_schemas',
+        'dts_test_app',
+        'django.contrib.contenttypes',
+    ))
+    @modify_settings(TENANT_APPS={
+        'append': 'django.contrib.contenttypes',
+    })
     def test_shared_apps_empty(self):
         self.assertBestPractice([
-            Warning("SHARED_APPS is empty.",
-                    id="tenant_schemas.W002"),
+            Warning("SHARED_APPS is empty.", id="tenant_schemas.W002"),
+            Error("You have INSTALLED_APPS that are not in either of "
+                  "TENANT_APPS or SHARED_APPS",
+                  hint=['tenant_schemas']),
         ])
 
     @override_settings(TENANT_APPS=(

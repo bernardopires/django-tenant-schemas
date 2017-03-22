07ebcc5f89474031d3204ec4dc88ba128192bf71
Stéphane Raimbault <stephane.raimbault@gmail.com>
2017-03-22 13:20:20 +0100

    Don't override default user or blocks Unix socket DB connection

--- dts_test_project/dts_test_project/settings.py
+++ dts_test_project/dts_test_project/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'tenant_schemas.postgresql_backend',
         'NAME': os.environ.get('PG_NAME', 'dts_test_project'),
-        'USER': os.environ.get('PG_USER', 'postgres'),
-        'PASSWORD': os.environ.get('PG_PASSWORD', 'root'),
-        'HOST': os.environ.get('PG_HOST', 'localhost'),
-        'PORT': int(os.environ.get('PG_PORT', '5432')),
+        'USER': os.environ.get('PG_USER'),
+        'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'HOST': os.environ.get('PG_HOST'),
+        'PORT': int(os.environ.get('PG_PORT')) if os.environ.get('PG_PORT') else None,
     }
 }
 

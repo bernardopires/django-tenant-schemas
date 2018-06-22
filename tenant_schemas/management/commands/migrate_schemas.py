e4cb908c0278a7c70b717f56bcee13a236cfb2b7
kingbuzzman <buzzi.javier@gmail.com>
2018-06-22 13:36:47 +0200

    Preliminary changes

--- tenant_schemas/management/commands/migrate_schemas.py
+++ tenant_schemas/management/commands/migrate_schemas.py
@@ -1,6 +1,8 @@
+import math
 import django
+import argparse
+
 from django.core.management.commands.migrate import Command as MigrateCommand
-from django.db import connection
 
 from tenant_schemas.management.commands import SyncCommon
 from tenant_schemas.migration_executors import get_executor
@@ -11,6 +13,22 @@ if django.VERSION >= (1, 9, 0):
 else:
     class MigrationSchemaMissing(django.db.utils.DatabaseError):
         pass
+
+
+def chunks(l, n):
+    for i in range(0, len(l), int(math.ceil(len(l) / n))):
+        yield l[i:i + n]
+
+
+def greater_than_x(min_number, message):
+    def wrapper(astring):
+        if not astring.isdigit():
+            raise argparse.ArgumentTypeError('Needs to be a number')
+        number = int(astring)
+        if not number > min_number:
+            raise argparse.ArgumentTypeError(message)
+        return number
+    return wrapper
 
 
 class Command(SyncCommon):
@@ -28,9 +46,26 @@ class Command(SyncCommon):
         super(Command, self).add_arguments(parser)
         command = MigrateCommand()
         command.add_arguments(parser)
+        parser.add_argument('--part', action='store', dest='migration_part',
+                            type=greater_than_x(1, 'The number needs to be greated than one'), default=None,
+                            help=('Splits the tenant schemas into pieces of equal parts to then be proccessed in '
+                                  'parts (requires --of).'))
+        parser.add_argument('--of', action='store', dest='migration_part_of',
+                            type=greater_than_x(0, 'The number needs to be greated than zero'), default=None,
+                            help='The part you want to process from the pieces (requires --part).')
 
     def handle(self, *args, **options):
         super(Command, self).handle(*args, **options)
+
+        required_together = (self.options['migration_part'], self.options['migration_part_of'],)
+        if any(required_together) and not all(required_together):
+            raise Exception("--part and --of need to be used together.")
+        elif all(required_together):
+            if self.options['migration_part_of'] > self.options['migration_part']:
+                raise Exception("--of cannot be greater than --part.")
+            elif self.sync_public:
+                raise Exception("Cannot run public schema migrations along with --of and --part.")
+
         self.PUBLIC_SCHEMA_NAME = get_public_schema_name()
 
         executor = get_executor(codename=self.executor)(self.args, self.options)
@@ -48,6 +83,9 @@ class Command(SyncCommon):
                 else:
                     tenants = [self.schema_name]
             else:
-                tenants = get_tenant_model().objects.exclude(schema_name=get_public_schema_name()).values_list(
-                    'schema_name', flat=True)
+                tenants = get_tenant_model().objects.exclude(schema_name=get_public_schema_name()) \
+                                            .order_by('pk').values_list('schema_name', flat=True)
+                if self.options['migration_part'] and self.options['migration_part'] > 1 and tenants:
+                    tenant_parts = list(chunks(tenants, self.options['migration_part'] - 1))
+                    tenants = tenant_parts[self.options['migration_part_of'] - 1]
             executor.run_migrations(tenants=tenants)

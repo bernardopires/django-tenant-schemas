1bd60631c6736c2d27f5bf25bacdc4139e4d710d
James Maroney <james.maroney@gmail.com>
2018-11-20 18:32:55 -0500

    only override createsuperuser command if get_user_model() returns a class with an app_label in settings.SHARED_APPS

--- tenant_schemas/management/commands/createsuperuser.py
+++ tenant_schemas/management/commands/createsuperuser.py
@@ -1,6 +1,14 @@
+from django.contrib.auth import get_user_model
 from tenant_schemas.management.commands import TenantWrappedCommand
 from django.contrib.auth.management.commands import createsuperuser
+from django.conf import settings
 
 
-class Command(TenantWrappedCommand):
+class WrappedCommand(TenantWrappedCommand):
     COMMAND = createsuperuser.Command
+
+
+user_model_in_shared_schema = get_user_model()._meta.app_label in settings.SHARED_APPS
+
+Command = createsuperuser.Command if user_model_in_shared_schema else WrappedCommand
+

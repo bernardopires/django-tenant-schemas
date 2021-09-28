3abfd6729b2f48013554dba45029ba51416b4a44
Vu Quang Hoa <joe.vqhoa@gmail.com>
2021-09-28 16:33:51 +0700

    Fix small issues

--- tenant_schemas/management/commands/collectstatic_schemas.py
+++ tenant_schemas/management/commands/collectstatic_schemas.py
@@ -2,4 +2,6 @@ from tenant_schemas.management.commands import BaseTenantCommand
 
 
 class Command(BaseTenantCommand):
+    requires_system_checks = []
+
     COMMAND_NAME = 'collectstatic'
--- tenant_schemas/management/commands/tenant_command.py
+++ tenant_schemas/management/commands/tenant_command.py
@@ -5,6 +5,7 @@ from tenant_schemas.management.commands import InteractiveTenantOption
 
 
 class Command(InteractiveTenantOption, BaseCommand):
+    requires_system_checks = []
     help = "Wrapper around django commands for use with an individual tenant"
 
     def handle(self, command, schema_name, *args, **options):

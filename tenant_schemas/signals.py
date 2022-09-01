732b8a88a209d91fabfe53f492c54007f04e73ee
Jeroen Brouwer <jeroen.brouwer@projectsone.nl>
2022-09-01 10:16:48 +0200

    support for django4.1

--- tenant_schemas/signals.py
+++ tenant_schemas/signals.py
@@ -1,6 +1,10 @@
 from django.dispatch import Signal
 
-post_schema_sync = Signal(providing_args=['tenant'])
+try:
+    # Django < 4.0
+    post_schema_sync = Signal(providing_args=['tenant'])
+except TypeError:
+    post_schema_sync = Signal()
 post_schema_sync.__doc__ = """
 Sent after a tenant has been saved, its schema created and synced
 """
